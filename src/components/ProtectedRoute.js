1a6e5b8ff4e8a26f318f3086d98b293abc7b3e67
alain17-web <web2020.alain@gmail.com>
2022-09-11 22:39:34 +0200

    auth firebase

---
+++ src/components/ProtectedRoute.js
@@ -0,0 +1,17 @@
+import { Navigate } from "react-router-dom"
+import { useUserAuth } from "../context/UserAuthContext"
+
+const ProtectedRoute = ({ children }) => {
+
+    let {user} = useUserAuth()
+
+    if(!user){
+       return <Navigate to='/'/>
+    }
+    else{
+        return children
+    }
+    
+}
+
+export default ProtectedRoute
